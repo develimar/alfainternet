261d17ce7a6a827093c5e54a61f975751ed92c71
develimar <elimar.pc@gmail.com>
2022-05-08 11:17:06 -0300

    Inserção do Principal COntroller e COnfiguração da Rota Inicial

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+//Route::get('/', function () {
+//    return view('index');
+//});
+
+//Route::get('/', function (){
+//   return view('site.index');
+//});
+
+Route::get('/', [PrincipalController::class, 'Principal'])->name('site.index');
